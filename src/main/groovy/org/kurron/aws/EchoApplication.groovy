8a6965e7a121dfda3ff9f086bd6d570376a50247
Ron Kurr <kurr@kurron.org>
2017-11-26 19:26:56 -0500

    Weed out dead classes.

--- src/main/groovy/org/kurron/aws/EchoApplication.groovy
+++ src/main/groovy/org/kurron/aws/EchoApplication.groovy
@@ -3,19 +3,12 @@ package org.kurron.aws
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
 class EchoApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run EchoApplication, args
-	}
-
-	@Bean
-	@Profile( ['failing-health'] )
-	HealthCheck healthCheck() {
-		new HealthCheck()
 	}
 
 	@Bean
